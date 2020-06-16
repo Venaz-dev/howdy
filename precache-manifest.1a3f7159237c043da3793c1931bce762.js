da27c41847a390fb88b154a24fede7e537673c91
Venaz-dev <57592712+Venaz-dev@users.noreply.github.com>
2020-06-16 19:11:12 +0100

    Updates

---
+++ precache-manifest.1a3f7159237c043da3793c1931bce762.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a469e8b09cf17c063f5b763b41094e8",
+    "url": "/howdy/index.html"
+  },
+  {
+    "revision": "eb5da74519f07aa4b6ae",
+    "url": "/howdy/static/css/main.65beb66d.chunk.css"
+  },
+  {
+    "revision": "c0b6a9c840e4e2f82a41",
+    "url": "/howdy/static/js/2.b98707d4.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/howdy/static/js/2.b98707d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5da74519f07aa4b6ae",
+    "url": "/howdy/static/js/main.0d66fb1a.chunk.js"
+  },
+  {
+    "revision": "381287b6519b7ffc8cbd",
+    "url": "/howdy/static/js/runtime-main.c6244576.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/howdy/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "302d32ab0402b531a4fb2f332385bf19",
+    "url": "/howdy/static/media/burger-menu.302d32ab.png"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/howdy/static/media/cancel.576f26ff.svg"
+  }
+]);
